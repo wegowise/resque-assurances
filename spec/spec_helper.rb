b17a3e4f3a928dc2ab7daf808284cc7bcc669cbf
Nathan Fixler <nathan@fixler.org>
2014-06-11 18:24:04 -0700

    Add a redis namespace for tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,8 @@ require_relative 'support/test_worker'
 require_relative 'support/unique_worker'
 require_relative 'support/another_unique_worker'
 
+Resque.redis.namespace = 'resque:test'
+
 RSpec.configure do |config|
   config.before(:each) do
     ResqueSpec.reset!
